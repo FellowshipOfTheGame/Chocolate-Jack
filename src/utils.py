b9d9d8ebfe43680a01d8b8866b2c8759e652d00a
iuryalves <iuryalves20@gmail.com>
2014-03-23 23:20:10 -0300

    args faz mais sentido

--- src/utils.py
+++ src/utils.py
@@ -7,8 +7,8 @@ __author__ = 'iury'
 def safe_load(function, *args):
     import sys
     if 'linux' in sys.platform:
-        path = args[0].replace('\\','/')
+        args = args[0].replace('\\','/')
     else:
-        path = args[0].replace('/','\\')
+        args = args[0].replace('/','\\')
 
-    return function(path)
+    return function(args)

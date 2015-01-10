550b586ae549892ab5437dc0fce8fc58a15bfb37
PauloPrado <pvsprado@gmail.com>
2015-01-10 18:53:23 -0200

    Adicionando projeto no stencyl

---
+++ StencylVersion/ChocoJack/behavior-hx/Design_4_4_Walking.hx
@@ -0,0 +1,138 @@
+package scripts;
+
+import com.stencyl.graphics.G;
+import com.stencyl.graphics.BitmapWrapper;
+
+import com.stencyl.behavior.Script;
+import com.stencyl.behavior.ActorScript;
+import com.stencyl.behavior.SceneScript;
+import com.stencyl.behavior.TimedTask;
+
+import com.stencyl.models.Actor;
+import com.stencyl.models.GameModel;
+import com.stencyl.models.actor.Animation;
+import com.stencyl.models.actor.ActorType;
+import com.stencyl.models.actor.Collision;
+import com.stencyl.models.actor.Group;
+import com.stencyl.models.Scene;
+import com.stencyl.models.Sound;
+import com.stencyl.models.Region;
+import com.stencyl.models.Font;
+
+import com.stencyl.Engine;
+import com.stencyl.Input;
+import com.stencyl.Key;
+import com.stencyl.utils.Utils;
+
+import nme.ui.Mouse;
+import nme.display.Graphics;
+import nme.display.BlendMode;
+import nme.display.BitmapData;
+import nme.display.Bitmap;
+import nme.events.Event;
+import nme.events.KeyboardEvent;
+import nme.events.TouchEvent;
+import nme.net.URLLoader;
+
+import box2D.common.math.B2Vec2;
+import box2D.dynamics.B2Body;
+import box2D.dynamics.B2Fixture;
+import box2D.dynamics.joints.B2Joint;
+
+import motion.Actuate;
+import motion.easing.Back;
+import motion.easing.Cubic;
+import motion.easing.Elastic;
+import motion.easing.Expo;
+import motion.easing.Linear;
+import motion.easing.Quad;
+import motion.easing.Quart;
+import motion.easing.Quint;
+import motion.easing.Sine;
+
+import com.stencyl.graphics.shaders.BasicShader;
+import com.stencyl.graphics.shaders.GrayscaleShader;
+import com.stencyl.graphics.shaders.SepiaShader;
+import com.stencyl.graphics.shaders.InvertShader;
+import com.stencyl.graphics.shaders.GrainShader;
+import com.stencyl.graphics.shaders.ExternalShader;
+import com.stencyl.graphics.shaders.InlineShader;
+import com.stencyl.graphics.shaders.BlurShader;
+import com.stencyl.graphics.shaders.SharpenShader;
+import com.stencyl.graphics.shaders.ScanlineShader;
+import com.stencyl.graphics.shaders.CSBShader;
+import com.stencyl.graphics.shaders.HueShader;
+import com.stencyl.graphics.shaders.TintShader;
+import com.stencyl.graphics.shaders.BloomShader;
+
+
+
+class Design_4_4_Walking extends ActorScript
+{          	
+	
+public var _rightB:String;
+
+public var _walkRA:String;
+
+public var _leftB:String;
+
+public var _walkLA:String;
+
+public var _idleRA:String;
+
+public var _walking:Bool;
+
+ 
+ 	public function new(dummy:Int, actor:Actor, engine:Engine)
+	{
+		super(actor, engine);	
+		nameMap.set("rightB", "_rightB");
+nameMap.set("walkRA", "_walkRA");
+nameMap.set("leftB", "_leftB");
+nameMap.set("walkLA", "_walkLA");
+nameMap.set("idleRA", "_idleRA");
+nameMap.set("walking", "_walking");
+_walking = false;
+nameMap.set("Actor", "actor");
+
+	}
+	
+	override public function init()
+	{
+		    
+/* ======================== When Updating ========================= */
+addWhenUpdatedListener(null, function(elapsedTime:Float, list:Array<Dynamic>):Void
+{
+if(wrapper.enabled)
+{
+        if(isKeyDown(_rightB))
+{
+            actor.setXVelocity(20);
+            _walking = true;
+propertyChanged("_walking", _walking);
+}
+
+        else if(isKeyDown(_leftB))
+{
+            actor.setXVelocity(-20);
+            _walking = true;
+propertyChanged("_walking", _walking);
+}
+
+        else
+{
+            actor.setXVelocity(0);
+            _walking = false;
+propertyChanged("_walking", _walking);
+}
+
+}
+});
+
+	}	      	
+	
+	override public function forwardMessage(msg:String)
+	{
+		
+	}
+}
